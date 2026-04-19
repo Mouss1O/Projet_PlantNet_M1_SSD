7d74a2a03160b9be4d0fe704897eb7ef98dc4365
firdakarimou2@gmail.com <firdakarimou2@gmail.com>
2026-04-19 16:48:35 +0200

    Nettoyage : suppression des doublons

--- verification.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Vérifier une observation au hasard
-scores = pd.read_csv("fichiers_utilises/ai_scores_all.csv")
-print("Aperçu des scores :")
-print(scores.head(10))
-
-print("\nStatistiques des scores :")
-print(scores['score'].describe())
